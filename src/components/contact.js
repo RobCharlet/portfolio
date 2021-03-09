282158c54b3af1dab0367e6070b97958553d2c52
Robin Charlet <robin.charlet@laposte.net>
2021-03-09 18:08:57 +0100

    Style form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,18 +1,89 @@
 import React from 'react';
+import { css } from '@emotion/react';
+import { mq } from '../utils/styles';
 
 const ContactForm = () => {
   return (
-    <form method="post" action="/contact">
-      <label htmlFor="name">Name:</label>
-      <input id="name" type="text" autocomplete="name" />
-      <label htmlFor="email">Email:</label>
-      <input email id="email" type="text" autocomplete="email" />
-      <label htmlFor="subject">Email:</label>
-      <input id="subject" type="text" autocomplete="subject" />
-      <label htmlFor="message">Enter your message:</label>
-      <textarea id="message" autocomplete="message"></textarea>
-      <button type="submit">Send</button>
-      <input type="reset" name="reset" value="Clear" />
+    <form
+      method="post"
+      action="https://getform.io/f/f546bb5f-57b7-4fa3-9cc9-2be7bb2798ff"
+      css={css`
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        margin-bottom: 1.5rem;
+
+        div {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          margin-bottom: 1rem;
+        }
+
+        input,
+        textarea {
+          border-width: 1px;
+          padding-top: 0.5rem;
+          padding-bottom: 0.5rem;
+          padding-left: 0.75rem;
+          padding-right: 0.75rem;
+          color: #3d4852;
+        }
+
+        label {
+          text-transform: uppercase;
+          font-weight: 700;
+          font-size: 1.125rem;
+          color: #3d4852;
+          margin-bottom: 0.5rem;
+        }
+
+        button {
+          display: block;
+          color: #ffffff;
+          background-color: #4dc0b5;
+          text-transform: uppercase;
+          font-size: 1.125rem;
+          padding: 1rem;
+          margin-left: auto;
+          margin-right: auto;
+          border-radius: 0.25rem;
+
+          &:hover {
+            background-color: #38a89d;
+          }
+        }
+
+        .half {
+          width: 100%;
+          ${mq[1]} {
+            width: 48%;
+          }
+        }
+
+        .full {
+          width: 100%;
+        }
+      `}
+    >
+      <div class="half">
+        <label htmlFor="name">Nom</label>
+        <input id="name" type="text" autocomplete="name" />
+      </div>
+      <div class="half">
+        <label htmlFor="email">Courriel</label>
+        <input email id="email" type="text" autocomplete="email" />
+      </div>
+      <div class="full">
+        <label htmlFor="subject">Sujet</label>
+        <input id="subject" type="text" autocomplete="subject" />
+      </div>
+      <div class="full">
+        <label htmlFor="message">Votre message</label>
+        <textarea id="message" autocomplete="message" rows="6"></textarea>
+      </div>
+
+      <button type="submit">Envoyer</button>
     </form>
   );
 };
